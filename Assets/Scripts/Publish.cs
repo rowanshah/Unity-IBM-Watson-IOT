0231fd3e3b89d9cacbdd7f039c64a5a1b274cf8f
Rowan Shah <rowan.shah@ibm.com>
2019-08-06 22:23:01 +0100

    Initial commit

---
+++ Assets/Scripts/Publish.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using IBMWIoTP;
+
+public class Publish : MonoBehaviour
+{
+    public string orgId;
+    public string appId;
+    public string apiKey;
+    public string authToken;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // enter your device details here
+        orgId = "";
+        appId = "";
+        apiKey = "";
+        authToken = "";
+
+        ApplicationClient applicationClient = new ApplicationClient(orgId, appId, apiKey, authToken);
+        try{
+            applicationClient.connect();
+            Debug.Log("Connected");
+        }
+        catch(Exception ex){
+            Debug.Log(ex);
+        }
+        /* try{
+            applicationClient.eventCallback += processEvent;
+            applicationClient.subscribeToDeviceEvents("ChildApp", "123456");
+        }
+        catch(Exception ex){
+            Debug.Log(ex);
+        }*/
+
+        try{
+            // change details here
+            applicationClient.publishCommand("device type", "device ID", "event Name", "format", "data", 2);
+        }
+        catch(Exception ex){
+            Debug.Log(ex);
+        }
+
+    }
+    public static void processEvent(string deviceType,string deviceId ,string eventName, string format, string data) {
+        Debug.Log(eventName + " " + data);
+        return;
+    }
+}
---
+++ Assets/Scripts/Subscribe.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using IBMWIoTP;
+
+public class Subscribe : MonoBehaviour
+{
+    public string orgId;
+    public string deviceType;
+    public string deviceId;
+    public string authToken;
+    public DeviceClient deviceClient;
+
+    private static string mood;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // enter your device details here
+        mood = "";
+        orgId = "";
+        deviceType = "";
+        deviceId = "";
+        authToken = "";
+
+        
+        deviceClient = new DeviceClient(orgId,deviceType,deviceId,"token",authToken);
+        /* try{
+            
+            deviceClient.publishEvent("current_emotion", "json", mood , 2);
+            Debug.Log("connected");
+        }
+        catch(Exception ex){
+            Debug.Log(ex);
+        }*/
+      
+
+        
+        try{
+            deviceClient.connect();
+            // change details here 
+            deviceClient.subscribeCommand("event name", "format", 2);
+            deviceClient.commandCallback += processCommand;
+        }
+        catch(Exception ex){
+            Debug.Log(ex);
+        }
+        
+        
+        
+    }
+
+    public static void processCommand(string cmdName, string format, string data) {
+        Debug.Log("Processed Command");
+        Debug.Log(cmdName + " " + data);
+        if (cmdName == "set_emotion"){
+            mood = data;
+        }
+        Debug.Log(mood);
+    }
+}
